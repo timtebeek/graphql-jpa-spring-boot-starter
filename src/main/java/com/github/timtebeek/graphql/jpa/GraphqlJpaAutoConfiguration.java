0115386db31faf1876915510a48ca5c0fe05f113
Tim te Beek <timtebeek@gmail.com>
2016-10-27 16:50:31 +0200

    Add @ComponentScan to discover GraphQLController

--- src/main/java/com/github/timtebeek/graphql/jpa/GraphqlJpaAutoConfiguration.java
+++ src/main/java/com/github/timtebeek/graphql/jpa/GraphqlJpaAutoConfiguration.java
@@ -5,9 +5,11 @@ import javax.persistence.EntityManager;
 import org.crygier.graphql.GraphQLExecutor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 @SuppressWarnings("static-method")
 public class GraphqlJpaAutoConfiguration {
 	@Bean
